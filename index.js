8b89ae2da41e2fc3a6e7662d94d99e84f624ce10
Your Name <you@example.com>
2023-05-05 11:26:17 +0300

    modification

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+let save_entry = document.getElementById("save_entry")
 let count_people = document.getElementById("count_people")
 let count = 0
 function increment(){
     count = count+1
-count_people.innerText = count
+count_people.textContent = count
 }
 function save(){
+    countStr = count + " - "
+    save_entry.textContent += countStr
     console.log(count)
+    count_people.textContent = 0
+    count = 0
 }
